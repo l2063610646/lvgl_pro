20932906970b752cb38320406a21970238b9321d
liu <2063610646@qq.com>
2026-07-29 12:11:46 +0800

    优化布局

--- widgets/ha_heading_card/ha_heading_card.c
+++ widgets/ha_heading_card/ha_heading_card.c
@@ -53,18 +53,11 @@ void refresh(lv_obj_t *obj)
     lv_obj_update_layout(root->badges_ctn);
     lv_obj_update_layout(root->main);
 
+    int32_t DEFAULT_TITLE_CTN_WIDTH = 150;
     int32_t t1 = lv_obj_get_width(root->title_icon);
     int32_t t2 = lv_obj_get_width(root->title_next);
     int32_t t3 = lv_obj_get_style_pad_column(root->title_row, 0);
     int32_t t4 = lv_obj_get_content_width(root->title_label);
-    if(t1 + t2 + t3 *2 + t4 > 150) {
-        int32_t temp = 150 - t3 * 2 - t2 - t1;
-        lv_obj_set_width(root->title_label, temp);
-        lv_obj_set_height(root->title_label, 16);
-        lv_label_set_long_mode(root->title_label, LV_LABEL_LONG_DOT);
-        lv_obj_update_layout(root->title_label);
-        t4 = lv_obj_get_content_width(root->title_label);
-    }
 
     int32_t title_ctn_width = t1 + t2 + t3 *2 + t4;
     int32_t badges_ctn_width = lv_obj_get_width(root->badges_ctn);
@@ -72,14 +65,32 @@ void refresh(lv_obj_t *obj)
 
     int32_t gap = lv_obj_get_style_pad_column(root->main, 0);
     if (title_ctn_width + badges_ctn_width + gap > main_ctn_width) {
-        // 超出父元素宽度，调整尺寸
-        int32_t target_title_ctn_width = title_ctn_width > 150 ? 150 : title_ctn_width;
-        lv_obj_set_width(root->title_ctn, target_title_ctn_width);
-        lv_obj_set_width(root->title_row, target_title_ctn_width);
-        lv_obj_set_style_clip_corner(root->title_ctn, true, 0);
-        int32_t target_badges_ctn_width = main_ctn_width - gap - target_title_ctn_width;
-        lv_obj_set_width(root->badges_ctn, target_badges_ctn_width);
+        if (main_ctn_width - gap - badges_ctn_width > DEFAULT_TITLE_CTN_WIDTH) {
+            // 说明title_ctn宽了，badges窄了，需要保持badges的宽度，调整title_ctn的宽度
+            int32_t remaining_title_ctn_width = main_ctn_width - gap - badges_ctn_width;
+            lv_obj_set_width(root->title_ctn, remaining_title_ctn_width);
+            lv_obj_set_width(root->title_row, remaining_title_ctn_width);
+            lv_obj_set_style_clip_corner(root->title_ctn, true, 0);
 
+            int32_t temp = remaining_title_ctn_width - t3 * 2 - t2 - t1;
+            lv_obj_set_width(root->title_label, temp);
+            lv_obj_set_height(root->title_label, 16);
+            lv_label_set_long_mode(root->title_label, LV_LABEL_LONG_DOT);
+        } else {
+            // 说明badges宽了，title_ctn窄了，需要限制title_ctn的最小宽度，调整badges的宽度
+            int32_t target_title_ctn_width = title_ctn_width > DEFAULT_TITLE_CTN_WIDTH ? DEFAULT_TITLE_CTN_WIDTH : title_ctn_width;
+            lv_obj_set_width(root->title_ctn, target_title_ctn_width);
+            lv_obj_set_width(root->title_row, target_title_ctn_width);
+            lv_obj_set_style_clip_corner(root->title_ctn, true, 0);
+
+            int32_t temp = DEFAULT_TITLE_CTN_WIDTH - t3 * 2 - t2 - t1;
+            lv_obj_set_width(root->title_label, temp);
+            lv_obj_set_height(root->title_label, 16);
+            lv_label_set_long_mode(root->title_label, LV_LABEL_LONG_DOT);
+
+            int32_t target_badges_ctn_width = main_ctn_width - gap - target_title_ctn_width;
+            lv_obj_set_width(root->badges_ctn, target_badges_ctn_width);
+        }
     }
 
     int32_t max_scroll_x = lv_obj_get_scroll_right(root->badges_ctn);
